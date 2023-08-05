2d1d3b8f3b1b74c8f85079df5ef673afd3148e04
AlejandroRuizSumelzo <71427257+AlejandroRuizSumelzo@users.noreply.github.com>
2023-08-05 20:45:01 +0200

    footer okey, mobile and desktop

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faMapLocationDot,
+  faPhoneVolume,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -11,11 +14,20 @@ import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+  mail: string = 'pastyboll@pastyboll.es';
+  faWhatsapp = faWhatsapp;
+  faInstagram = faInstagram;
+  faMaps = faMapLocationDot;
+  faPhone = faPhoneVolume;
+  faMail = faEnvelope;
 
   constructor(library: FaIconLibrary) {
     library.addIcons(
       faWhatsapp,
-      faInstagram
+      faInstagram,
+      faMapLocationDot,
+      faPhoneVolume,
+      faEnvelope
     );
   }
 }
