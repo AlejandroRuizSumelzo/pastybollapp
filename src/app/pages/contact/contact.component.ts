2366c20c0145b6f7578715eb4b8fdd24d4afb216
AlejandroRuizSumelzo <71427257+AlejandroRuizSumelzo@users.noreply.github.com>
2023-08-08 18:09:25 +0200

    contact page okey

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faMapLocationDot,
+  faPhoneVolume,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +14,20 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  mail: string = 'pastyboll@pastyboll.es';
+  faWhatsapp = faWhatsapp;
+  faInstagram = faInstagram;
+  faMaps = faMapLocationDot;
+  faPhone = faPhoneVolume;
+  faMail = faEnvelope;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faWhatsapp,
+      faInstagram,
+      faMapLocationDot,
+      faPhoneVolume,
+      faEnvelope
+    );
+  }
 }
