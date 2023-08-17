f151714562e1d24febae26883496324910dde30e
AlejandroRuizSumelzo <71427257+AlejandroRuizSumelzo@users.noreply.github.com>
2023-08-17 18:04:33 +0200

    update images carousel

--- src/app/component/carousel/carousel.component.ts
+++ src/app/component/carousel/carousel.component.ts
@@ -5,7 +5,7 @@ import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-carousel',
   standalone: true,
-	imports: [NgIf, CarouselModule],
+  imports: [NgIf, CarouselModule],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
@@ -20,25 +20,27 @@ export class CarouselComponent {
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       400: {
-        items: 1
+        items: 1,
       },
       740: {
-        items: 1
+        items: 1,
       },
       940: {
-        items: 1
-      }
+        items: 1,
+      },
     },
-    nav: false
-  }
+    nav: false,
+  };
   images: string[] = [];
-  
+
   constructor() {
-    this.images[0] = "../../../assets/img/home_carousel/europastry_logo.png";
-    this.images[1] = "../../../assets/img/home_carousel/okin_logo.png";
-    this.images[2] = "../../../assets/img/home_carousel/frida_logo.png";
+    this.images[0] = '../../../assets/img/home_carousel/europastry_logo.webp';
+    this.images[1] = '../../../assets/img/home_carousel/okin_logo.webp';
+    this.images[2] = '../../../assets/img/home_carousel/frida_logo.webp';
+    this.images[3] = '../../../assets/img/home_carousel/barcos_logo.webp';
+    this.images[4] = '../../../assets/img/home_carousel/idella_logo.webp';
   }
 }
